23c71b1ce7fee93d24f210b370c8952bae3db360
jeferson2205 <jeferson.adriano12@gmail.com>
2022-03-11 23:48:29 -0300

    Fist commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+        System.out.println(gato);
+
+        /*- a = 5;
+        int b = 4;
+        System.out.println("hello oi" + (a,b));*/
+
+    }
+
+}
